44e3f1ed25c17c1b4fe409c52b3f2c38f515a603
Roman <rvslisenko@gmail.com>
2020-06-05 13:06:46 +0300

    fixes

--- app/js/custom.js
+++ app/js/custom.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
 
     $('.my-calendar').calendar();
 
+    $('.info-message .close').on('click', function(event) {
+        event.preventDefault();
+        $(this).closest('.info-message').addClass('d-none');
+    });
 });
