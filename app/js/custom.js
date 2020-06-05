0bbf88ede365fd1920b5019e42d4b370dec3107e
Roman <rvslisenko@gmail.com>
2020-06-05 16:02:55 +0300

    mobile header updates

--- app/js/custom.js
+++ app/js/custom.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     $('.my-calendar').calendar();
 
     $('body').css('paddingTop', $('.info-message').outerHeight(true));
+    $('html, body').animate({
+        scrollTop: 0
+    }, 800);
 
     $('.info-message .close').on('click', function(event) {
         event.preventDefault();
